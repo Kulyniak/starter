c09fc093793fb853b83783adfc32def6ea4ffad3
Kulyniak <80394011+Kulyniak@users.noreply.github.com>
2022-10-10 19:56:31 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// code chalenge one
+const weightMarkkg = 78;
+const weightJoneskg = 92;
+const heightMarkcm = 169;
+const heightJonescm = 195;
+const markBMI = weightMarkkg /( heightMarkcm *2)
+const jonesBMI = weightJoneskg/ (heightJonescm *2)
+const markhigherBMI = markBMI > jonesBMI
+console.log(markhigherBMI)
